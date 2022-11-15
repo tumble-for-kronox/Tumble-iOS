b45d0318e333340a3da1409c83d0f1963271b6ab
adisve <adis.veletanlic@gmail.com>
2022-11-16 00:12:24 +0100

    Initial Commit

---
+++ tumble-ios/tumble_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tumble_iosApp.swift
+//  tumble-ios
+//
+//  Created by Adis Veletanlic on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct tumble_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
