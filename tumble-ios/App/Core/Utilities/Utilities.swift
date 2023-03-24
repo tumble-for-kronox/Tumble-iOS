167cb65e6bba6113105f3b0f39099eeec72ddfb1
adisve <adis.veletanlic@gmail.com>
2023-03-24 11:42:09 +0100

    Change dateformatters to var

--- tumble-ios/App/Core/Utilities/Utilities.swift
+++ tumble-ios/App/Core/Utilities/Utilities.swift
@@ -36,47 +36,47 @@ func getCurrentDate(truncate: Bool = false) -> String {
 /// Formatters used globally
 
 /// To convert API result date (ISO8601) to `Date`
-let isoDateFormatterFract: ISO8601DateFormatter = {
+var isoDateFormatterFract: ISO8601DateFormatter = {
     let formatter = ISO8601DateFormatter()
     formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
     return formatter
 }()
 
 /// Used on event dates
-let dateFormatterEvent: DateFormatter = {
+var dateFormatterEvent: DateFormatter = {
     let eventDateFormatter = DateFormatter()
     eventDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
     return eventDateFormatter
 }()
 
-let dateFormatterComma: DateFormatter = {
+var dateFormatterComma: DateFormatter = {
     let formatter = DateFormatter()
     formatter.locale = Locale(identifier: "en_US_POSIX")
     formatter.dateFormat = "EEEE, MMMM d, yyyy"
     return formatter
 }()
 
-let dateFormatterFull: DateFormatter = {
+var dateFormatterFull: DateFormatter = {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
     return dateFormatter
 }()
 
-let dateFormatterSemi: DateFormatter = {
+var dateFormatterSemi: DateFormatter = {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd"
     return dateFormatter
 }()
 
-let isoDateFormatterDashed: ISO8601DateFormatter = {
+var isoDateFormatterDashed: ISO8601DateFormatter = {
     let formatter = ISO8601DateFormatter()
     formatter.formatOptions = [.withFullDate, .withDashSeparatorInDate]
     return formatter
 }()
 
-let isoDateFormatter: ISO8601DateFormatter = ISO8601DateFormatter()
+var isoDateFormatter: ISO8601DateFormatter = ISO8601DateFormatter()
 
-let dateFormatterLong: DateFormatter = {
+var dateFormatterLong: DateFormatter = {
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: "en_US")
     dateFormatter.dateStyle = .long
