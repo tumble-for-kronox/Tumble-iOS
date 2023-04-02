8b0ab8ff3087bc58da95a09439e2a8c821fc8a57
adisve <adis.veletanlic@gmail.com>
2023-04-02 12:41:24 +0200

    Create symlink to google service plist and change script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/zsh
 
 #  ci_post_clone.sh
 #  tumble-ios
 #
 #  Created by Adis Veletanlic on 4/2/23.
-#  
+#
 
-# Define the path to the GoogleService-Info.plist file in your source code
-GOOGLE_PLIST_PATH="../tumble-ios/GoogleService-Info.plist"
+# Define the path to the symbolic link to the GoogleService-Info.plist file in your source code
+GOOGLE_PLIST_PATH="GoogleService-Info.plist"
 
 # Define the destination path in the Xcode Cloud build environment
 DESTINATION_PATH="$HOME/Library/MobileDevice/Provisioning\ Profiles/"
