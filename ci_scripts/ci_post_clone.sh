66681157c62f2e9b491044445bf091c049e50539
adisve <adis.veletanlic@gmail.com>
2023-04-02 12:55:43 +0200

    Edit post-clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,11 +6,8 @@
 #  Created by Adis Veletanlic on 4/2/23.
 #
 
-# Define the path to the symbolic link to the GoogleService-Info.plist file in your source code
+# Define the path to the GoogleService-Info.plist file in your source code
 GOOGLE_PLIST_PATH="GoogleService-Info.plist"
 
-# Define the destination path in the Xcode Cloud build environment
-DESTINATION_PATH="$HOME/Library/MobileDevice/Provisioning\ Profiles/"
-
-# Copy the file to the build environment
-cp -r "$GOOGLE_PLIST_PATH" "$DESTINATION_PATH"
+# Copy the file to the tumble-ios directory
+cp -r "$GOOGLE_PLIST_PATH" ../tumble-ios/
