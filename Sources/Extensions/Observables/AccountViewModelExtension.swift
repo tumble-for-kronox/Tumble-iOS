8794fccef642aeb5e45725eaf99eb61588831ece
adisve <adis.veletanlic@gmail.com>
2023-05-05 11:07:39 +0200

    Fix persistent bug where notification for booking is not removed with the booking. Now simply the individual ID of the booking is used as the notification ID

--- Sources/Extensions/Observables/AccountViewModelExtension.swift
+++ Sources/Extensions/Observables/AccountViewModelExtension.swift
@@ -11,8 +11,6 @@ extension AccountViewModel {
     func scheduleBookingNotifications(for bookings: Response.KronoxUserBookings) {
         DispatchQueue.global(qos: .background).async { [weak self] in
             guard let self else { return }
-            // Remove all old notifications in case booking has been changed externally or internally
-            self.notificationManager.cancelNotifications(with: "Booking")
             for booking in bookings {
                 if let notification = self.notificationManager.createNotificationFromBooking(booking: booking) {
                     self.notificationManager.scheduleNotification(
@@ -22,7 +20,7 @@ extension AccountViewModel {
                         completion: { result in
                             switch result {
                             case .success(let success):
-                                AppLogger.shared.info("Scheduled \(success) notification")
+                                AppLogger.shared.info("Scheduled \(success) notification with id: \(notification.id)")
                             case .failure(let failure):
                                 AppLogger.shared.debug("Failed : \(failure)")
                             }
@@ -36,7 +34,6 @@ extension AccountViewModel {
     }
     
     func registerAutoSignup(
-        tries: Int = 0,
         completion: @escaping (Result<Void, Error>) -> Void
     ) {
         AppLogger.shared.debug("Automatically signing up for exams")
