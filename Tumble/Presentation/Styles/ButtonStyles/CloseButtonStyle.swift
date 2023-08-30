6c8b4d2724f193ac9a30eb8c2d1d3eb2ab0a7be4
Adis Veletanlic <96535657+adisve@users.noreply.github.com>
2023-08-30 14:10:36 +0200

    Fix News sheet UI on iPhone pro max series

---
+++ Tumble/Presentation/Styles/ButtonStyles/CloseButtonStyle.swift
@@ -0,0 +1,22 @@
+//
+//  CloseButtonStyle.swift
+//  Tumble
+//
+//  Created by Adis Veletanlic on 2023-08-30.
+//
+
+import Foundation
+import SwiftUI
+
+struct CloseButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(10)
+            .background(Color.surface)
+            .cornerRadius(25)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+    
+}
